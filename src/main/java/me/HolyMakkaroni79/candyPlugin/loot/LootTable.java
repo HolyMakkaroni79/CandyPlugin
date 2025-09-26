575928ea12892370ecbeed434cddbf492387e329
Toni Kordik <Toni.Kordik@gmail.com>
2025-09-26 15:19:15 +0200

    -Changes the candys to apples, -Added a var for the drop changes -Deleted the debugging information -deleted the if statment with the return

--- src/main/java/me/HolyMakkaroni79/candyPlugin/loot/LootTable.java
+++ src/main/java/me/HolyMakkaroni79/candyPlugin/loot/LootTable.java
@@ -1,28 +1,36 @@
 package me.HolyMakkaroni79.candyPlugin.loot;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Random;
 
 public class LootTable implements Listener {
+
+    public int minVal = 1; //inklusiv
+    public int maxVal = 4; //exklusiv
+    public double dropChange = 0.2; //chance to drop a candy (for example: 0.5 = 50%) only 10 steps (10%; 20%; 30%)
+
+    ItemStack candy = new ItemStack(Material.COOKIE);
+    ItemMeta meta = candy.getItemMeta();
     @EventHandler
     public void onMobDeath(EntityDeathEvent event){
-        if(event.getEntity().getLastDamageCause() == null || !(event.getEntity().getLastDamageCause().getEntity() instanceof Player)){
-            Bukkit.broadcastMessage("kein plan why aber n entity ist tot");
-            //return;
-        }
 
         if(event.getEntity().getKiller() instanceof Player){
             Random random = new Random();
-            Bukkit.broadcastMessage("Achso du warst des");
             var chance = random.nextDouble();
-            if(chance >= 0.5){
-                Bukkit.broadcastMessage("joa irgendwie hast du dep es jetzt geschaft n zufalls gen zu baun. Frag mich bitte nicht wie du es geschaft hast");
-
+            if(chance <= dropChange){
+                int randomModelData = random.nextInt(minVal, maxVal);
+                meta.setCustomModelData(randomModelData);
+                candy.setItemMeta(meta);
+                event.getEntity().getKiller().getInventory().addItem(candy);
             }
         }
     }
